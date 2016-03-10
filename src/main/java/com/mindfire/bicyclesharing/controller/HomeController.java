b3d3f60427ed359108b3af7eca08824fe57204fc
Ashish Roy <ashish.roy@mindfiresolutions.com>
2016-03-10 16:52:37 +0530

    Added Registration and Login Page

--- src/main/java/com/mindfire/bicyclesharing/controller/HomeController.java
+++ src/main/java/com/mindfire/bicyclesharing/controller/HomeController.java
@@ -10,4 +10,14 @@ public class HomeController {
 	public String index(){
 		return "index";
 	}
+	
+	@RequestMapping("register")
+	public String reg(){
+		return "registration";
+	}
+	
+	@RequestMapping("login")
+	public String signIn(){
+		return "signIn";
+	}
 }
