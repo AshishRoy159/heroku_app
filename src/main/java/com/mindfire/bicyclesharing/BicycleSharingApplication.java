932d9a11352866062a4e896f2af84a6e16dcc611
Ashish Roy <ashish.roy@mindfiresolutions.com>
2016-03-10 11:17:28 +0530

    Completed Project Setup

---
+++ src/main/java/com/mindfire/bicyclesharing/BicycleSharingApplication.java
@@ -0,0 +1,12 @@
+package com.mindfire.bicyclesharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicycleSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicycleSharingApplication.class, args);
+	}
+}
